79f9b93bae6068aec6494a34bcfef58539a175f6
Maksym Havryliuk <maks.havryliuk@gmail.com>
2018-04-19 08:31:03 +0300

    Exception - adding the solution of the third task.

---
+++ Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.IO;
+
+/* Write a program that enters file name along with its full file path (e.g. C:\WINDOWS\win.ini), 
+ * reads its contents and prints it on the console. Find in MSDN how to use System.IO.File.ReadAllText(…). 
+ * Be sure to catch all possible exceptions and print user-friendly error messages.
+ * 
+ * Напишите программу, которая вводит имя файла вместе с полным адресом к файлу (например, C: \ WINDOWS \ win.ini),
+ * считывает его содержимое и печатает его на консоли. Найти в MSDN, как использовать System.IO.File.ReadAllText (...).
+ * Обязательно воспользуйтесь всеми исключениями и напечатайте понятные сообщения об ошибках.
+ * https://msdn.microsoft.com/ru-ru/library/system.exception(v=vs.110).aspx */
+
+namespace _20180306_Task3_Exception
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Enter filename and path to him (e.g. C:\\WINDOWS\\win.ini)");
+            string file = Console.ReadLine();
+
+            try
+            {
+                Console.WriteLine(File.ReadAllText(file));
+            }
+            catch (DriveNotFoundException ex)
+            {
+                Console.WriteLine("Error: " + ex.Message);
+                Console.WriteLine("A drive is unavailable or does not exist.");
+                Console.WriteLine("Привод недоступен или не существует.");
+            }
+            catch (DirectoryNotFoundException ex)
+            {
+                Console.WriteLine("Error: " + ex.Message);
+                Console.WriteLine("Part of a directory path is not valid.");
+                Console.WriteLine("Недопустимая часть пути к каталогу.");
+            }
+            catch (FileNotFoundException ex)
+            {
+                Console.WriteLine("Error: " + ex.Message);
+                Console.WriteLine("A file does not exist.");
+                Console.WriteLine("Файл не существует.");
+            }
+            catch (PathTooLongException ex)
+            {
+                Console.WriteLine("Error: " + ex.Message);
+                Console.WriteLine("A path or file name exceeds the maximum system-defined length.");
+                Console.WriteLine("Путь или имя файла превышает максимальную системную длину.");
+            }
+            catch (PlatformNotSupportedException ex)
+            {
+                Console.WriteLine("Error: " + ex.Message);
+                Console.WriteLine("The operation is not supported on the current platform.");
+                Console.WriteLine("Операция не поддерживается на текущей платформе.");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error: " + ex.Message);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
